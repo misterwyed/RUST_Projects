60f9734f3cbdafe3203f8819356971485e0fff74
misterwyed <mayanksingh37@gmail.com>
2024-01-24 15:15:23 +0530

    The Scanner has been added

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
+import java.util.*;
+
 class Hello {
     public static void main(String[] args) {
         System.out.println("Helllo, there!!!");
         System.out.println("Hello this is a java program");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
     }
 }
