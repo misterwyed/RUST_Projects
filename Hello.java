38c8078b812f9829b356e1bd726081b6ba67eec1
misterwyed <mayanksingh37@gmail.com>
2024-01-24 14:03:58 +0530

    A new line added to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
-class Hello{
-public static void main(String []args){
-System.out.println("Hello this is a java program");
-}
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Helllo, there!!!");
+        System.out.println("Hello this is a java program");
+    }
 }
